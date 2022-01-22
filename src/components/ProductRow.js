23e655b384cf21ecb691d65178107192158d2f04
evanina <eva.degano@gmail.com>
2022-01-22 15:23:02 +0100

    iteration 1

---
+++ src/components/ProductRow.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ProductRow extends React.Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default ProductRow;
---
+++ src/components/ProductTable.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductRow from "./ProductRow";
+
+class ProductTable extends React.Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default ProductTable;
